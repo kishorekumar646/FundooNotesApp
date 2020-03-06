7d70414e8330b70dafb787236b623f7cc8cd8986
kishorekumar646 <kishorekumar131646@gmail.com>
2020-03-06 19:33:08 +0530

    [kishore]Add.added serializer.py file

---
+++ fundooNotes/userForms/serializer.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from userForms.models import UserDetails,Notes
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+class RegisterationFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetails
+        fields = ['username','email','password','confirm_password']
+
+class LoginFormFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetails
+        fields = ['username','password']
+
+class ForgotPasswordFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetails
+        fields = ['email']
+
+class ResetPasswordFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetails
+        fields = ['password','confirm_password']
+
+class CreateNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['title','takeNote']
