f641359b572abe1c6c733aeebbf4b4dbf410cb5f
kishorekumar646 <kishorekumar131646@gmail.com>
2020-03-06 19:32:19 +0530

    [kishore]Add.added models.py file

---
+++ fundooNotes/userForms/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserDetails(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    confirm_password = models.CharField(max_length=20)
+
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    takeNote = models.TextField()
+    
