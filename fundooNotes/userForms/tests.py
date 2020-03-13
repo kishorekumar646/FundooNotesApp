61de4792170a45ec18e312d929efca53a14daa29
kishorekumar646 <kishorekumar131646@gmail.com>
2020-03-13 12:02:02 +0530

    [kishore]Add.added tests.py file from userForms app

---
+++ fundooNotes/userForms/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.test import APITestCase
+from fundooNotes.settings import BASE_URL
+from collections.abc import Mapping, Sequence
+from . import views
+
+import unittest
+import pytest
+
+client = Client()
+
+
+@pytest.mark.django_db
+class test_TestUrls(unittest.TestCase):
+
+    def test_RegistarationOnSubmit_ThenReturn_HTTP_406_NOT_ACCEPTABLE(self):
+        url = BASE_URL + reverse("register")
+        userData = {'username': '', 'email': '',
+                    'password': '', 'confirm_password': ''}
+        response = client.post(path=url, data=userData, format='json')
+
+        self.assertEqual(response.status_code, 406)
