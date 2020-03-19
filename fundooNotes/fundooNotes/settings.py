ad726c581bbe4ba2531b5ec4974da357d004f12b
kishorekumar646 <kishorekumar131646@gmail.com>
2020-03-19 10:05:23 +0530

    [kishore]Refactor. adding environment variables for email config in settings.py

--- fundooNotes/fundooNotes/settings.py
+++ fundooNotes/fundooNotes/settings.py
@@ -48,11 +48,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kishorekumar131646@gmail.com'
-EMAIL_HOST_PASSWORD = 'Kishorekumar@646'
-EMAIL_PORT = '587'
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
 LOGIN_URL = '/login/'
 
