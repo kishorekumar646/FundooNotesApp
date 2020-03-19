6a5a1e842b74ee7a6c4069c360a3e1ce19af43d0
kishorekumar646 <kishorekumar131646@gmail.com>
2020-03-19 10:07:37 +0530

    [kishore]Refactor. adding environment variables for AWS config for storing pics in settings.py file

--- fundooNotes/fundooNotes/settings.py
+++ fundooNotes/fundooNotes/settings.py
@@ -59,6 +59,14 @@ LOGIN_URL = '/login/'
 
 BASE_URL = 'http://127.0.0.1:8000'
 
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
